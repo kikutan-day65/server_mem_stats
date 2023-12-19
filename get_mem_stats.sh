3227227b11f9611aceb32a18bbd08a737a304299
haruto-ubuntu <haruto@haruto-VirtualBox>
2023-12-19 20:09:01 +0900

    feat: add fuctions to get each mem info and convert into gb

--- get_mem_stats.sh
+++ get_mem_stats.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
 
+function mbToGb() {
+    while read -r line; do
+        gb=$(echo "scale=2; $line / 1024" | bc) # 小数点2位まで表示
+        echo "${gb}"
+    done
+}
+
+function kbToGb() {
+    while read -r line; do
+        gb=$(echo "scale=2; $line / 1048576" | bc) # 小数点2位まで表示
+        echo "${gb}"
+    done
+}
+
+# function freeMem() {
+    
+# }
+
+HOST_NAME=$(memshow | grep host | awk '{print $3}')
+AVAILABLE_MEM=$(memshow | grep available_mem | awk '{print $3}' | sed 's/mb//g' | mbToGb)
+ASSIGNED_MEM=$(memshow | grep assigned_mem | awk '{print $3}' | sed 's/kb//g' | kbToGb)
+
+
+paste <(echo "${HOST_NAME}") <(echo "${AVAILABLE_MEM}") <(echo "${ASSIGNED_MEM}")
